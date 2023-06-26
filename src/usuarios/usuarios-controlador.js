bcbd4a584103898a8460ddda084cfdcace50095a
KatiaSonally <katiasonally@yahoo.com.br>
2023-06-26 18:01:57 -0300

    implements creating the token and sending it to the user with the JSON Web Token

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -1,5 +1,17 @@
 const Usuario = require('./usuarios-modelo');
 const { InvalidArgumentError, InternalServerError } = require('../erros');
+const usuarios = require('.');
+
+const jwt = require ('jsonwebtoken');
+
+function criaTokenJWT(usuario) {
+  const payload = {
+    id: usuario.id
+  };
+
+  const token = jwt.sign(payload, 'senha-secreta');
+  return token;
+}
 
 module.exports = {
   adiciona: async (req, res) => {
@@ -28,6 +40,8 @@ module.exports = {
   },
 
   login: (req, res) => {
+    const token = criaTokenJWT(req.user);
+    res.set('Authorization', token);
     res.status(204).send();
   },
 
