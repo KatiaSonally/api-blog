22a92a3d981710f3788effc91771303cdcfdf15c
KatiaSonally <katiasonally@yahoo.com.br>
2023-06-29 12:29:47 -0300

    checks code error

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -2,24 +2,48 @@ const passport = require('passport');
 
 module.exports = {
     local: (req, res, next) => {
-        passport.authenticate('local', 
-        { session: false }, 
+        passport.authenticate(
+            'local', 
+            { session: false }, 
+            (erro, usuario, info) => {
+                if (erro && erro.name === 'InvalidArgumentError') {
+                    return res.status(401).json({ erro: erro.message });
+                }
+
+                if (erro) {
+                    return res.status(500).json({ erro: erro.massage });
+                }
+
+                if (!usuario) {
+                    return res.status(401).json();
+                }
+
+                req.user = usuario;
+                return next();
+            }
+        )(req, res, next);
+  },
+
+  bearer: (req, res, next) => {
+    passport.authenticate(
+        'bearer',
+        { session: false },
         (erro, usuario, info) => {
-            if (erro && erro.name === 'InvalidArgumentError') {
+            if (erro && erro.name === 'JsonWebTokenError') {
                 return res.status(401).json({ erro: erro.message });
             }
 
             if (erro) {
-                return res.status(500).json({ erro: erro.massage });
+                return res.status(500).json({ erro: erro.message });
             }
 
-            if (!usuario) {
+            if(!usuario) {
                 return res.status(401).json();
             }
 
             req.user = usuario;
             return next();
         }
-      )(req, res, next);
+    )
   }
 };
