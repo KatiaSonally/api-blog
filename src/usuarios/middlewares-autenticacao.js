85e0a772e1697a3e6da9e359f20a3d44f330640d
KatiaSonally <katiasonally@yahoo.com.br>
2023-07-12 12:04:37 -0300

    implements token expiry

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -33,11 +33,11 @@ module.exports = {
                     return res.status(401).json({ erro: erro.message });
                 }
 
-                /*if (erro && erro.name === 'TokenExpiredError') {
+                if (erro && erro.name === 'TokenExpiredError') {
                     return res
                     .status(401)
                     .json({ erro: erro.message, expiradoEm: erro.expiredAt});
-                }*/
+                }
 
                 if (erro) {
                     return res.status(500).json({ erro: erro.message });
